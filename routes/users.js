b2bb5e47ebffe542512fe1147ca10caa7669783b
clevin m.wilson <clevinmwilson@gmail.com>
2021-09-11 20:24:06 +0530

    signup page added

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,10 @@ router.get('/', function(req, res, next) {
 router.get('/login',(req,res)=>{
   res.render('user/login')
 })
+
+
+//signup page
+router.get('/signup',(req,res)=>{
+  res.render('user/signup')
+})
 module.exports = router;
