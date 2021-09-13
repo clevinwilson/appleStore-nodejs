7a3fe8d3fd6402e7bf0e5bfcd181d01ee2887d01
clevin m.wilson <clevinmwilson@gmail.com>
2021-09-13 12:31:43 +0530

    product helpers added  for product

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var productHelper=require('../helpers/product-helpers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
