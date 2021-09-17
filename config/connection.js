36827358bc98181830efabe98879dd5d8decb1f6
clevin m.wilson <clevinmwilson@gmail.com>
2021-09-17 11:48:25 +0530

    connection,package-lock, files updated

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ module.exports.connect=function(done){
     const url='mongodb://localhost:27017'
     const dbname='applestore'
 
-    mongoClient.connect(url,(err,data)=>{
+    mongoClient.connect(url,{useUnifiedTopology: true},(err,data)=>{
         if(err) return done(err)
         state.db=data.db(dbname)
         done()
