caf03364eaa03df1ae3531fe1add64ca3bc79e59
clevin m.wilson <clevinmwilson@gmail.com>
2021-09-20 16:50:42 +0530

    user login and log out function added

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -8,14 +8,37 @@ module.exports={
         return new Promise(async(resolve,reject)=>{
             var email=await db.get().collection(collection.USER_COLLECTION).findOne({email:signUpDetails.email})
            if(email){
-           console.log(email);
+           resolve({status:false})
            }else{
             signUpDetails.joindate=new Date();
             signUpDetails.password= await bcrypt.hash(signUpDetails.password, 10)
             db.get().collection(collection.USER_COLLECTION).insertOne(signUpDetails).then((response)=>{
-                resolve(response)
+                resolve({status:true})
             })
            }
+        })
+    },
+    doLogin:(details)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.USER_COLLECTION).findOne({email:details.email}).then((user)=>{
+                if (user) {
+                    bcrypt.compare(details.password, user.password).then((status) => {
+                        if (status) {
+                            response.user = user
+                            response.status = true
+                            resolve(response)
+                        } else {
+                            response.status=false;
+                            response.message="Your account information was entered incorrectly.";
+                            resolve(response)
+                        }
+                    })
+                } else {
+                    response.status=false;
+                    response.message="User not exists";
+                    resolve(response)
+                }
+            })
         })
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -4,28 +4,69 @@ var router = express.Router();
 var productHelper=require('../helpers/product-helpers')
 var userHelper=require('../helpers/user-helpers')
 
+const verifyLogin = (req, res, next) => {
+  if (req.session.loggedIn) {
+      next();
+  } else {
+      res.redirect("/login");
+  }
+};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('user/index')
+  console.log(req.session.user);
+    res.render('user/index',{user:req.session.user});
+
 });
 
+//login 
 router.get('/login',(req,res)=>{
-  res.render('user/login')
+  if( req.session.loggedIn){
+    res.redirect('/');
+  }else{
+  res.render('user/login',{loginError:req.session.loginError})
+  req.session.loginError=false;
+  }
+})
+
+router.post('/signin',(req,res)=>{
+  userHelper.doLogin(req.body).then((response)=>{
+    if(response.status){
+      req.session.loggedIn=true;
+      req.session.user=response.user;
+      res.redirect('/');
+    }else{
+      req.session.loginError=response.message
+      res.redirect('/login')
+    }
+  })
+})
+
+//logout
+router.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/')
 })
 
 
 //signup page
 router.get('/signup',(req,res)=>{
+  if( req.session.loggedIn){
+    res.redirect('/');
+  }else{
   res.render('user/signup',{signupError:req.session.signupError})
   req.session.signupError=false;
+  }
 })
 
 router.post('/signup',(req,res)=>{
  if(req.body.firstname.toString().length && req.body.lastname.toString().length && req.body.email.toString().length && req.body.password.toString().length && req.body.password.toString().length > 3){
   userHelper.doSignup(req.body).then((response)=>{
-    if(response){
+    if(response.status){
       res.json({status:true})
     }else{
+      req.session.signupError="Something went wrong or User already exists "
+      console.log('false');
       res.json({status:false})
     }
   })
@@ -37,24 +78,24 @@ router.post('/signup',(req,res)=>{
 
 //index page
 router.get('/imac',(req,res)=>{
-  res.render('user/imac')
+  res.render('user/imac',{user:req.session.user})
 })
 
 //iphone page 
 router.get('/iphone',(req,res)=>{
   productHelper.getPhones().then((products)=>{
-    res.render('user/iphone',{products:products})
+    res.render('user/iphone',{products:products,user:req.session.user})
   })
 })
 
 //iphone 12 page
 router.get('/iphone-12',(req,res)=>{
-  res.render('user/iphone-12')
+  res.render('user/iphone-12',{user:req.session.user})
 })
 
 //buy product page 
 router.get('/buy-product',(req,res)=>{
-  res.render('user/buy-product')
+  res.render('user/buy-product',{user:req.session.user})
 })
 
 module.exports = router;
